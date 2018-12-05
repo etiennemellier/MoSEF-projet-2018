ef35d4a806ce9e0578bdde841c5acd649848fe1d
Etienne Mellier <etienne.mellier@gmail.com>
2018-12-05 20:27:22 +0100

    Ajouter le fichier

---
+++ fich_semaine_derniere.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+date=$(date)
+echo "Bienvenue $USERNAME, nous sommes le $date."
+cd ~
+ls -l
+read -p "Quel répertoire vous intéresse aujourd'hui?" path
+echo "Le répertoire est bien $path/"
+cd $path
+
+echo "find -type f mtime -7"" >> fich_semaine_derniere.sh
+
